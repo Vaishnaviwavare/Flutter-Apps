7b1232c2dfe06add959a978e966659cb112b6312
Vaishnavi wavare <140233622+Vaishnaviwavare@users.noreply.github.com>
2024-10-09 20:11:27 +0530

    Update main.dart

--- class app/stateless app/column_with_scroll/lib/main.dart
+++ class app/stateless app/column_with_scroll/lib/main.dart
@@ -9,10 +9,36 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text(
+            "Column Demo",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.network(
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn8KQmm94ZHg8-EgW8VqeqILsL89OmVCBXmg&s"),
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.amber,
+              ),
+              Image.network(
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn8KQmm94ZHg8-EgW8VqeqILsL89OmVCBXmg&s"),
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.amber,
+              ),
+            ],
+          ),
         ),
       ),
     );
